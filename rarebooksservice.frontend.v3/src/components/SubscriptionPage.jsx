c8b9773e483cc38fff4d9966698b41b4f392e4c3
doomkinn-geek <doomkinn@yandex.ru>
2025-01-23 21:07:00 +0300

    ...

--- rarebooksservice.frontend.v3/src/components/SubscriptionPage.jsx
+++ rarebooksservice.frontend.v3/src/components/SubscriptionPage.jsx
@@ -46,7 +46,11 @@ const SubscriptionPage = () => {
     return (
         <div className="container">
             <h2>Оформить подписку</h2>
-            <p>Стоимость подписки: 1000 руб/месяц</p>
+            <p>Стоимость подписки: </p>
+            <p>500 руб/месяц - 50 запросов в месяц</p>
+            <p>1000 руб/месяц - 200 запросов в месяц</p>
+            <p>1500 руб/месяц - 400 запросов в месяц</p>
+            <p>2000 руб/месяц - полный доступ</p>
             {error && <ErrorMessage message={error} />}
             {loading ? <p>Создание платежа...</p> : <button onClick={handleSubscribe}>Перейти к оплате</button>}
         </div>
