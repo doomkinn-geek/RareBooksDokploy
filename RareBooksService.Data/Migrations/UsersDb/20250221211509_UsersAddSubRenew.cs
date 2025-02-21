b557d909bae9f2ae2cc5e7e36699333a95d7149d
doomkinn-geek <doomkinn@yandex.ru>
2025-02-22 00:15:31 +0300

    добавлена миграция для автоматического создания платежа

---
+++ RareBooksService.Data/Migrations/UsersDb/20250221211509_UsersAddSubRenew.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RareBooksService.Data.Migrations.UsersDb
+{
+    /// <inheritdoc />
+    public partial class UsersAddSubRenew : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethodId",
+                table: "Subscriptions",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethodId",
+                table: "Subscriptions");
+        }
+    }
+}
